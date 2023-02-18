5b3ee801c55b1dc3b812e8f567a2d168b581540a
savindu29 <1savindupanagoda@gmail.com>
2023-02-18 20:50:03 +0530

    init

---
+++ src/main/java/com/savindu/POS/EzyPOS/EzyPosApplication.java
@@ -0,0 +1,13 @@
+package com.savindu.POS.EzyPOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzyPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzyPosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/savindu/POS/EzyPOS/EzyPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savindu.POS.EzyPOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzyPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
