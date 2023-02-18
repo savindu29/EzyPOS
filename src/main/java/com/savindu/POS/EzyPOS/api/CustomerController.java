faa63800057455b10de90ebbcb6855bc1a9c66f7
savindu29 <1savindupanagoda@gmail.com>
2023-02-19 03:41:54 +0530

    added anotations to CustomerDto

--- src/main/java/com/savindu/POS/EzyPOS/api/CustomerController.java
+++ src/main/java/com/savindu/POS/EzyPOS/api/CustomerController.java
@@ -1,25 +1,31 @@
 package com.savindu.POS.EzyPOS.api;
 
 import com.savindu.POS.EzyPOS.dto.request.CustomerDto;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/customer")
 public class CustomerController {
 
-    public String save(CustomerDto dto){
+    @PostMapping("/create") // localhost:8000/api/v1/customer/create (POST)
+    public String save(@PathVariable CustomerDto dto){
         return dto.toString();
     }
-    public String find(String id){
+    @GetMapping("/{id}") // localhost:8000/api/v1/customer/15 (GET)
+    public String find(@PathVariable String id){
         return id+" - Customer";
     }
-    public String updateCustomer(CustomerDto dto,String id){
+
+    @PutMapping(value = "/modify",params = {"id"}) // localhost:8000/api/v1/customer/modify?id=15 (PUT)
+    public String updateCustomer(@RequestBody CustomerDto dto,@RequestParam String id){
         return dto.toString()+" "+id+" -Updated";
     }
-    public String deleteCustomer(String id){
+
+    @DeleteMapping("/remove/{id}") // localhost:8000/api/v1/customer/remove/15 (DELETE)
+    public String deleteCustomer(@PathVariable String id){
         return id+ "-Deleted";
     }
+
+    @GetMapping("/list") // localhost:8000/api/v1/customer/list (GET)
     public String findAllCustomer(){
         return "All Customers";
     }
