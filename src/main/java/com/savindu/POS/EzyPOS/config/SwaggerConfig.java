cd041234cb25577a83a6e2ca67c0b6c9d75b6514
savindu29 <1savindupanagoda@gmail.com>
2023-02-19 01:32:08 +0530

    changed spring version

--- src/main/java/com/savindu/POS/EzyPOS/config/SwaggerConfig.java
+++ src/main/java/com/savindu/POS/EzyPOS/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+
 public class SwaggerConfig {
     @Bean
     public Docket initSwagger(){
